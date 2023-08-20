c5688eb0d99099093ad80cc2cadd4afc2890efb2
navneet-chugh <navneetchugh15@gmail.com>
2023-08-20 18:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const colorSelect = document.getElementById("colorSelect");
+  const removeButton = document.querySelector('input[type="button"][value="Select and Remove"]');
+
+  removeButton.addEventListener("click", function() {
+    const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    if (selectedOption) {
+      colorSelect.removeChild(selectedOption);
+    }
+  });
+});
